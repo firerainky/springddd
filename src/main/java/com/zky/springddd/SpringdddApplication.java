8d003a37b2e586be1b2bee468cc81a08de8bbbd2
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-12-03 18:51:06 +0800

    Added init spring code

---
+++ src/main/java/com/zky/springddd/SpringdddApplication.java
@@ -0,0 +1,13 @@
+package com.zky.springddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zky/springddd/SpringdddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zky.springddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
